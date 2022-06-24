aa4f11a961a1d96d64752b949083b8231bbc43a3
leonardo <leonardommascarenhas@gmail.com>
2022-06-23 21:22:46 -0300

    criação de canva

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const grid = document.querySelector(".sketch");
-
-function createRow() {
-  for (let i = 1; i <= 16; i++) {
-    const htmlString = document.createElement("div");
-    htmlString.classList.add("bla");
-    htmlString.innerHTML = `${i}`;
-    grid.appendChild(htmlString);
+function criadorGrid(size) {
+  let canva = document.querySelector(".canva");
+  canva.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  canva.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  for (let i = 0; i < 256; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    canva.insertAdjacentElement("beforeend", pixel);
   }
 }
 
-function createColumn() {
-  let i = 0;
-  while (i < 16) {
-    createRow();
-    i++;
+const mudaCor = document.querySelector(".canva");
+
+mudaCor.addEventListener("mousemove", mudacor);
+
+function mudacor(e) {
+  if (e.target.classList.contains("pixel")) {
+    let cor = document.querySelector(".color");
+    e.target.style.backgroundColor = cor.value;
   }
 }
 
-createColumn();
+function limpar() {
+  let canva = document.querySelector(".canva");
+  canva.innerHTML = "";
+  criadorGrid(16);
+}
+const botao = document.querySelector(".reset");
+botao.addEventListener("click", limpar);
+criadorGrid(16);
