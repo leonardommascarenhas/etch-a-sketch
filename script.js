af7d84a0b04c964501dbe5940eaa727886a11b20
leonardo <leonardommascarenhas@gmail.com>
2022-06-24 21:20:53 -0300

    código completo, falta css

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
+let color = "black";
+
 function criadorGrid(size) {
   let canva = document.querySelector(".canva");
   canva.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   canva.style.gridTemplateRows = `repeat(${size} , 1fr)`;
   for (let i = 0; i < 256; i++) {
     let pixel = document.createElement("div");
-    pixel.classList.add("pixel");
+    pixel.addEventListener("mousemove", mudacor);
+    pixel.backgroundColor = "white";
     canva.insertAdjacentElement("beforeend", pixel);
   }
 }
 
-const mudaCor = document.querySelector(".canva");
-
-mudaCor.addEventListener("mousemove", mudacor);
-
-function mudacor(e) {
-  if (e.target.classList.contains("pixel")) {
-    let cor = document.querySelector(".color");
-    e.target.style.backgroundColor = cor.value;
-  }
+function mudacor() {
+  this.style.backgroundColor = color;
 }
-
+function escolhaCor(escolha) {
+  color = escolha;
+}
 function limpar() {
   let canva = document.querySelector(".canva");
   canva.innerHTML = "";
